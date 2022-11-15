4bdcf27fd0c6d3c047110e7c8efaeac487fd12aa
YosophAhmed <devyosoph@gmail.com>
2022-11-16 00:13:25 +0200

    Response Status Code

--- lib/services/all_categories_service.dart
+++ lib/services/all_categories_service.dart
@@ -2,13 +2,16 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-
 class AllCategoriesService {
   Future<List<dynamic>> getAllCategories() async {
     http.Response response = await http.get(
       Uri.parse('https://fakestoreapi.com/products/categories'),
     );
-    List<dynamic> data = jsonDecode(response.body);
-    return data;
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception('Status Code Problem ${response.statusCode}');
+    }
   }
 }
--- lib/services/all_products_service.dart
+++ lib/services/all_products_service.dart
@@ -8,13 +8,18 @@ class AllProductsService {
     http.Response response = await http.get(
       Uri.parse('https://fakestoreapi.com/products'),
     );
-    List<dynamic> data = jsonDecode(response.body);
-    List<ProductModel> productsList = [];
-    for (int i = 0; i < data.length; i++) {
-      productsList.add(
-        ProductModel.fromJson(data[i]),
-      );
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      List<ProductModel> productsList = [];
+      for (int i = 0; i < data.length; i++) {
+        productsList.add(
+          ProductModel.fromJson(data[i]),
+        );
+      }
+      return productsList;
+    } else {
+      throw Exception('Status Code Problem ${response.statusCode}');
     }
-    return productsList;
   }
 }
