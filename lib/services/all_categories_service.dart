1a960aaf45b5483923861497ea8e9d008c38aa8e
YosophAhmed <devyosoph@gmail.com>
2022-11-16 00:00:05 +0200

    Product By Category Service

--- lib/services/all_categories_service.dart
+++ lib/services/all_categories_service.dart
@@ -4,7 +4,6 @@ import 'package:http/http.dart' as http;
 
 
 class AllCategoriesService {
-
   Future<List<dynamic>> getAllCategories() async {
     http.Response response = await http.get(
       Uri.parse('https://fakestoreapi.com/products/categories'),
