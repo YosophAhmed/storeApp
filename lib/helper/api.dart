94771a7f15283e994560149c74d0d21b8f1b5f80
YosophAhmed <devyosoph@gmail.com>
2022-11-17 15:10:24 +0200

    Update Product Service

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -4,8 +4,18 @@ import 'package:http/http.dart' as http;
 class Api {
   Future<dynamic> get({
     required String url,
+    String? token,
   }) async {
-    http.Response response = await http.get(Uri.parse(url));
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({
+        'Authorization': 'Bearer $token',
+      });
+    }
+    http.Response response = await http.get(
+      Uri.parse(url),
+      headers: headers,
+    );
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
@@ -21,6 +31,36 @@ class Api {
     String? token,
   }) async {
     Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({
+        'Authorization': 'Bearer $token',
+      });
+    }
+    http.Response response = await http.post(
+      Uri.parse(
+        url,
+      ),
+      body: body,
+      headers: headers,
+    );
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception(
+        'Status Code Problem ${response.statusCode} with body ${jsonDecode(response.body)}',
+      );
+    }
+  }
+
+  Future<dynamic> put({
+    required String url,
+    body,
+    String? token,
+  }) async {
+    Map<String, String> headers = {};
+    headers.addAll({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
     if (token != null) {
       headers.addAll({
         'Authorization': 'Bearer $token',
